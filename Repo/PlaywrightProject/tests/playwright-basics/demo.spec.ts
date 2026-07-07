b787182e178d32fa5883bbdc7a75606aecdde845
Playwright practice project <adarsh.ughade117@gmail.com>
2026-07-08 01:16:32 +0530

    one test case added-first commit

---
+++ Repo/PlaywrightProject/tests/playwright-basics/demo.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('verify title', async ({ page }) => {
+  await page.goto('https://www.letskodeit.com/practice');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Practice Page/);
+});
+
+test('verify taaluka title', async ({ page }) => {
+  await page.goto('https://taaluka.netlify.app/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Taaluka — आपल्या तालुक्याचे आपले ॲप/);
+});
+
+test('verify taaluka title2', async ({ page }) => {
+  await page.goto('https://taaluka.netlify.app/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Taaluka — आपल्या तालुक्याचे आपले ॲप/);
+});
